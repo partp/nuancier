beb32a2b9ec15ecad1555a6432d05c7c7a25adcd
Parth Panchal <parthpanchl@gmail.com>
2015-06-19 15:34:54 +0530

    Adds buttons to enlarge & shrink all the overlays simultaneously

--- nuancier/static/multimonitor.js
+++ nuancier/static/multimonitor.js
@@ -1,9 +1,16 @@
-var element_pos = 0;
-var overlays = [];
+var $cropImage = $('#cropImage'),
+    overlays = [],
+    patt = /-(\d+)x(\d+).\w{3,4}/g,
+    res = patt.exec($('#cropImage').attr('src')),
+    wallpaperWidth = res[1],
+    wallpaperHeight = res[2],
+    cropImageWidth = $cropImage.width(),
+    cropImageHeight = $cropImage.height();
+
 $(document).ready(function() {
     $('#addOverlay').click(function() {
-        var xAspect = $("#xAspect").val();
-        var yAspect = $("#yAspect").val();
+        var xAspect = $('#xAspect').val();
+        var yAspect = $('#yAspect').val();
         if(Number(xAspect) > 0 && Number(yAspect) > 0) {
             var dynamic_div = $(document.createElement('div')).css({
                 width: xAspect, height: yAspect
@@ -11,6 +18,26 @@ $(document).ready(function() {
             $(dynamic_div).addClass('overlays').draggable().resizable();
             $(dynamic_div).appendTo('#cropArea');
             overlays.push($(dynamic_div));
+        }
+    });
+    $('#enlargeOverlay').click(function() {
+        if(overlays.length > 0){
+            for(var i = 0; i < overlays.length; i++) {
+                overlays[i].css({
+                        'width' : overlays[i].width()  * 1.1,
+                        'height': overlays[i].height() * 1.1
+                });
+            }
+        }
+    });
+    $('#shrinkOverlay').click(function() {
+        if(overlays.length > 0){
+            for(var i = 0; i < overlays.length; i++) {
+                overlays[i].css({
+                        'width' : overlays[i].width()  / 1.1,
+                        'height': overlays[i].height() / 1.1
+                });
+            }
         }
     });
 });
